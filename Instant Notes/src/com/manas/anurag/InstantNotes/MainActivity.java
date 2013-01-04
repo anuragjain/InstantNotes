4663115209c21876f6c51536ee41a9200d443806
anuragjain <anuragjain1991@gmail.com>
2013-01-05 02:34:16 +0530

    basic functioning

--- Instant Notes/src/com/manas/anurag/InstantNotes/MainActivity.java
+++ Instant Notes/src/com/manas/anurag/InstantNotes/MainActivity.java
@@ -1,20 +1,164 @@
 package com.manas.anurag.InstantNotes;
 
-import android.os.Bundle;
+import java.util.Map;
+import java.util.Vector;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	ListView listView;
+	Vector<String> values = new Vector<String>();
+	ArrayAdapter<String> adapter ;
+	Map<String, ?> key_values;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		listView = (ListView) findViewById(R.id.mylist);
+
+		refreshListView();
+		listView.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				startActivity(new Intent(getApplicationContext(), DisplayNote.class));
+			}
+		}); 
+		registerForContextMenu(listView);
+	}
+
+	private void addNote() {
+		AlertDialog.Builder alert = new AlertDialog.Builder(this);
+		final EditText input = new EditText(this);
+		alert
+		.setTitle("Set Name")
+		.setView(input)
+		.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int whichButton) {
+				String value = input.getText().toString(); 
+				SharedPreferences settings = getSharedPreferences("myprefs",MODE_PRIVATE);
+				SharedPreferences.Editor editor = settings.edit();
+				editor.putString(value, value);
+				editor.commit();
+				refreshListView();
+			}
+		}).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int whichButton) {
+				// Do nothing.
+			}
+		}).show();
+	}
+
+	private void delete(MenuItem item) {
+		final AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+		String tabname = values.get(info.position);
+		SharedPreferences.Editor editor = getSharedPreferences("myprefs", MODE_PRIVATE).edit();
+		editor.remove(tabname);
+		editor.commit();
+		refreshListView();
+	}
+
+	private void refreshListView() {
+		SharedPreferences sp  = getSharedPreferences("myprefs", MODE_PRIVATE);
+		key_values = sp.getAll();
+
+		values.clear();
+		for(String key : key_values.keySet()){
+			values.add(key);
+		}
+
+		adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+		listView.setAdapter(adapter); 
+	}
+
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+		MenuInflater inflater = getMenuInflater();
+		if(v.getId() == R.id.mylist){
+			inflater.inflate(R.menu.listcontextmenu, menu);
+		}
+	}
+
+	public boolean onContextItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.rename:
+			renameListItem(item);
+			break;
+		
+		case R.id.delete:
+			delete(item);
+			break;
+		}
+		
+		return true;
+	}
+
+
+	private void renameListItem(MenuItem item) {
+		final AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+		AlertDialog.Builder alert = new AlertDialog.Builder(this);
+		final EditText input = new EditText(this);
+		alert
+		.setTitle("Update Name")
+		.setView(input)
+		.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int whichButton) {
+				String value = input.getText().toString(); 
+				SharedPreferences settings = getSharedPreferences("myprefs",MODE_PRIVATE);
+				SharedPreferences.Editor editor = settings.edit();
+				editor.remove(values.get(info.position));
+				editor.putString(value, value);
+				editor.commit();
+				refreshListView();
+			}
+		}).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int whichButton) {
+				// Do nothing.
+			}
+		}).show();
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle item selection
+		switch (item.getItemId()) {
+		case R.id.addnote:	
+			addNote();
+			return true;
+
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+
 }
